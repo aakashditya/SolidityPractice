6ee59b90d3e9cb87f2bb36fc6b83b59e9dfedfa6
aakashditya <44800796+aakashditya@users.noreply.github.com>
2022-08-30 15:26:31 +0530

    Update and rename FirstProgram.sol to Ballot.sol

--- Ballot.sol
+++ Ballot.sol
@@ -13,3 +13,20 @@ contract Inbox {
         return message;
         }
 }
+
+
+
+## edited
+pragma solidity ^0.4.17;
+
+contract Inbox {
+    string public message;
+    function Inbox(string initialMessage) public {
+        message = initialMessage;
+
+    }
+    function setMessage(string newMessage) public {
+        message = newMessage;
+    }
+   
+}
